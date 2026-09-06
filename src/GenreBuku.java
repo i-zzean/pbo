f37e6640c47c7dd18388d2322997c3b9f47a0e40
Sanmii1 <145213955+Sanmii1@users.noreply.github.com>
2026-09-06 09:52:44 +0700

    Update GenreBuku

--- src/GenreBuku.java
+++ src/GenreBuku.java
@@ -1,15 +1,28 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class GenreBuku {
+    private static List<GenreBuku> daftarGenre = new ArrayList<GenreBuku>();
+
     private String idGenre;
     private String namaGenre;
+
+    private static void addGenre(GenreBuku genre){
+        daftarGenre.add(genre);
+    };
 
     public GenreBuku(String namaGenre){
         this.idGenre = "" + Math.floor(Math.random()* 101);
         this.namaGenre = namaGenre;
+        addGenre(null);
     }
+
 
     public String getInfo(){
         return "id : " + idGenre + " Nama : " + namaGenre;
     };
+
+
 
 
 }
